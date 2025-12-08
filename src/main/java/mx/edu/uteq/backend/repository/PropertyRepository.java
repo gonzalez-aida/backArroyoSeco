a53ecb4500e1f8082c84be1b31cd917b68f69bf3
gonzalez-aida <aidalu13oct@gmail.com>
2025-12-07 19:13:55 -0600

    Bloqueo pesimista a nivel de base de datos

--- src/main/java/mx/edu/uteq/backend/repository/PropertyRepository.java
+++ src/main/java/mx/edu/uteq/backend/repository/PropertyRepository.java
@@ -2,14 +2,18 @@ package mx.edu.uteq.backend.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 import mx.edu.uteq.backend.model.Property;
 import java.util.List;
 
+import jakarta.persistence.LockModeType; 
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
+    
     Optional<Property> findByName(String name);
-    Optional<Property> findById(Long id);
+    @Lock(LockModeType.PESSIMISTIC_WRITE) 
+    Optional<Property> findById(Long id); 
     List<Property> findByOwnerId_Id(Long ownerId);
 }
